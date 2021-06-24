c25ebdd740be8158887947365e3960eb16000398
Lukasz Tlalka <lukasz.tlalka@netblink.net>
2021-06-24 21:11:36 +0000

    Wroking with curl

--- app/Server/Command.php
+++ app/Server/Command.php
@@ -8,11 +8,10 @@ class Command
     {
         return 'curl --http1.1 -s -N ' .
             '-H "Authorization: Bearer '.$ref.'" ' .
-            env('APP_URL').':'.env("APP_SERVER_PORT").'/outputClient | ' .
-            'bash | ' .
+            env('APP_URL').':'.env("APP_SERVER_PORT").'/inputClient | ' .
+            'script -q | ' .
             'curl -H "Transfer-Encoding: chunked" ' .
             '-H "Authorization: Bearer '.$ref.'"' .
-            //' -X POST -T - '.env('APP_URL').':'.env("APP_SERVER_PORT").'/console-share?inputClient='.$ref.' -s';
             ' -X POST -T - '.env('APP_URL').':'.env("APP_SERVER_PORT").'/outputClient';
     }
 }
